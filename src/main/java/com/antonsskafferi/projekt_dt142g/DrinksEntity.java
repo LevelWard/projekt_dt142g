748b1ad6839ea80861f8ac6c0cad978fadab0841
fredde00 <98824715+fredde00@users.noreply.github.com>
2023-12-08 13:57:30 +0100

    fixat att man kan lägga till, ta bort och uppdatera menyn och dryckeslistan.

--- src/main/java/com/antonsskafferi/projekt_dt142g/DrinksEntity.java
+++ src/main/java/com/antonsskafferi/projekt_dt142g/DrinksEntity.java
@@ -1,31 +1,24 @@
 package com.antonsskafferi.projekt_dt142g;
 
-import jakarta.enterprise.inject.Model;
 import jakarta.persistence.*;
 
 import java.util.Objects;
 
-@Model
-@NamedQuery(name = "dishesEntity.allData", query = "SELECT d FROM DishesEntity d")
 @Entity
-@Table(name = "dishes", schema = "asdb", catalog = "")
-public class DishesEntity {
+@Table(name = "drinks", schema = "asdb", catalog = "")
+public class DrinksEntity {
     @Id
-    @Column(name = "TITLE", nullable = true)
+    @Column(name = "TITLE")
     private String title;
     @Basic
-    @Column(name = "PRICE", nullable = true)
+    @Column(name = "PRICE")
     private Integer price;
     @Basic
-    @Column(name = "DESCRIPTION", nullable = true)
+    @Column(name = "DESCRIPTION")
     private String description;
     @Basic
-    @Column(name = "TYPE", nullable = true)
+    @Column(name = "TYPE")
     private String type;
-
-    @Basic
-    @Column(name = "SUBTYPE", nullable = true)
-    private String subtype;
 
     public String getTitle() {
         return title;
@@ -59,28 +52,23 @@ public class DishesEntity {
         this.type = type;
     }
 
-    public String getSubtype() { return subtype; }
-
-    public void setSubtype(String subtype) { this.subtype = subtype; }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        DishesEntity that = (DishesEntity) o;
+        DrinksEntity that = (DrinksEntity) o;
 
         if (title != null ? !title.equals(that.title) : that.title != null) return false;
         if (price != null ? !price.equals(that.price) : that.price != null) return false;
         if (description != null ? !description.equals(that.description) : that.description != null) return false;
         if (type != null ? !type.equals(that.type) : that.type != null) return false;
-        if (subtype != null ? !subtype.equals(that.subtype) : that.subtype != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, price, description, type, subtype);
+        return Objects.hash(title, price, description, type);
     }
 }
