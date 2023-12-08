4f3e5075062fb68eaccea74ea1759fc6fbb480ea
Iloonio <decadentlithium@gmail.com>
2023-12-08 15:31:40 +0100

    added comments and empty function to bookingsEntity.java. We need to make sure that these are implemented before we roll out the orderBean class

--- src/main/java/com/antonsskafferi/projekt_dt142g/OrderItem.java
+++ src/main/java/com/antonsskafferi/projekt_dt142g/OrderItem.java
@@ -1,12 +1,21 @@
 package com.antonsskafferi.projekt_dt142g;
 
+
 public class OrderItem {
-    public int amount;  //amount of item
-    public String name; //name of item
+    private int amount;  //amount of item
+    private String name; //name of item
 
     public OrderItem(int amount, String name){
         this.amount = amount;
         this.name = name;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public String getName(){
+        return name;
     }
 
 
--- src/main/java/com/antonsskafferi/projekt_dt142g/orderBean.java
+++ src/main/java/com/antonsskafferi/projekt_dt142g/orderBean.java
@@ -1,30 +1,70 @@
 package com.antonsskafferi.projekt_dt142g;
-
 import java.util.List;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
 
+@ApplicationScoped
+@Named
 public class orderBean {
 
+    //TODO: implement a function to send items to order_meals and order_drinks
     private List<OrderItem> itemsList;
-    //TODO: if the item already exists, increment OrderItem.amount :-) 🫡
-/*
-    public void push(int amount, String name){
-        OrderItem item = new OrderItem(amount, name);
-        if(!itemsList.contains(item)) {
-            itemsList.add(item);
-        }
-        else{
-            //find the index of existing item (based on string!!!)
-            //change that item's
+    private int table_id;
+
+    /**
+     * Pushes unique items into itemsList. if the items name already exists them we'll increment
+     * an existing item's amount by 1.
+     * @param new_item an item to be added to the list.
+     */
+    public void push(OrderItem new_item) {
+
+        if (!itemsList.contains(new_item)) {
+            itemsList.add(new_item);
+        } else {
+            for (OrderItem existingItem : itemsList) {
+                if (existingItem.getName().equals(new_item.getName())) {
+                    existingItem = new OrderItem(existingItem.getAmount() + 1, existingItem.getName());
+                }
+            }
         }
     }
-    */
 
-    /*
-      This bean will contain all meals and drinks that will go to a particular table.
-      Each unique meal or drink will be stored as a unique class called "OrderItem"
-
-      class OrderItem contains the following info:
-       -    int amount      // the amount of this item that will be ordered
-       - String name        // the name of the item
+    /**
+     *
+     * @return private itemsList containing OrderItems
      */
+    public List<OrderItem> getItemsList() {
+        return itemsList;
+    }
+
+
+    /**
+     * for each Meal from itemList:
+     * insert new item in table_orders (save its order_nr)
+     * insert new item in order_meals with order_nr, title, and amount included.
+     * pop the item.
+     */
+    public void insertMealOrders(){
+
+    }
+
+    /**
+     * for each drink from itemList:
+     * insert new item in table_orders (save its order_nr)
+     * insert new item in order_drinks with order_nr, title, and amount included.
+     * pop the item.
+     */
+    public void insertDrinkOrders(){
+
+    }
+
+    /**
+     * sends all items in list to the db, this should clear the list completely.
+     */
+    public void flushList(){
+        insertDrinkOrders();
+        insertMealOrders();
+        itemsList.clear();
+    }
+
 }
