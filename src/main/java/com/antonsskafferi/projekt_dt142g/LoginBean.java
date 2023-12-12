501977ec29bc8c5e5dbf03e99729a668a6e29f55
GianF00 <gico2100@student.miun.se>
2023-12-12 11:56:45 +0100

    implementerat en enkel inloggningsmekanism i LoginBean.

--- src/main/java/com/antonsskafferi/projekt_dt142g/LoginBean.java
+++ src/main/java/com/antonsskafferi/projekt_dt142g/LoginBean.java
@@ -1,6 +1,8 @@
 package com.antonsskafferi.projekt_dt142g;
 
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
+import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
@@ -17,28 +19,10 @@ import java.util.Objects;
 
 @Stateless
 @Named
+
 public class LoginBean{
     private String username;
     private String password;
-    public String login(){
-        /*ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-        try{
-            request.login(username, password);
-            return "/secured/main.xhtml?faces-redirect=true";
-        }catch (ServletException e){
-            return "AdminInlogg.xhtml?error=true&faces-redirect=true";
-        }*/
-
-
-        if("olle".equals(username) && "hej123".equals(password)){
-            return "main.xhtml";
-        }
-        else{
-            //.addMessage(null, new FacesMessage("Login failed"));
-            return "AdminInlogg.xhtml";
-        }
-    }
 
     public String getUsername(){
         return username;
@@ -52,5 +36,28 @@ public class LoginBean{
     public void setPassword(String password){
         this.password = password;
     }
+    /*Har skapat en login funktion där admin kan logga in */
+    public String login(){
+        /*ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+        try{
+            request.login(username, password);
+            return "/secured/main.xhtml?faces-redirect=true";
+        }catch (ServletException e){
+            return "AdminInlogg.xhtml?error=true&faces-redirect=true";
+        }*/
 
+        //Om Username och password stämmer överrens med Olle och hej123
+        //kan admin logga in och gå vidare till nästa sida
+        //Om det är fel får admin prova igen
+
+        if(Objects.equals(username, "olle") && Objects.equals(password, "hej123")){
+            return "main.xhtml";
+        }
+        else{
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Felaktig inloggning, ange rätt info", "Misslyckad inloggnig"));
+            return "AdminInlogg.xhtml";
+        }
+    }
 }
